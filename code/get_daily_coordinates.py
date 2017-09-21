7937bf198c3a5013c90dbb0bc97cf9f03d7d9500
Dan Saunders <djsaunde@umass.edu>
2017-09-21 08:16:07 -0400

    Working towards a memory-efficient criterion-satisfying data saving.

--- code/get_daily_coordinates.py
+++ code/get_daily_coordinates.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import argparse
+import xlsxwriter
 import pandas as pd
 import multiprocessing as mp
 
@@ -17,59 +18,63 @@ for directory in [ output_path, plots_path ]:
 
 
 def plot_and_record_daily(taxi_data, start_date, end_date):
+	wb = xlsxwriter.Workbook(os.path.join(output_path, '_'.join([ '_'.join(taxi_data.keys()), str(distance), start_date, end_date ])), {'constant_memory': True})
+
 	# set up main loop: loop through each day from start_date to end_date
-	with open(os.path.join(output_path, '_'.join([ '_'.join(taxi_data.keys()), str(distance), str(start_date), str(end_date) ])) + '.csv', 'w') as f:
-		for idx, date in enumerate(daterange(start_date, end_date)):
-			start_time = timeit.default_timer()
+	for idx, date in enumerate(daterange(start_date, end_date)):
+		start_time = timeit.default_timer()
 
-			# get coordinates of new distance and time-constraint satisfying taxicab trips with nearby pick-ups
-			all_coords = {}
-			for key in taxi_data.keys():
-				data = taxi_data[key]
+		# get coordinates of new distance and time-constraint satisfying taxicab trips with nearby pick-ups
+		all_coords = {}
+		for key in taxi_data.keys():
+			data = taxi_data[key]
 
-				# Create a number of CPU workers equal to the number of hotels in the data
-				pool = mp.Pool(n_jobs)
+			# Create a number of CPU workers equal to the number of hotels in the data
+			pool = mp.Pool(n_jobs)
 
-				# Get a list of the names of the hotels we aim to plot distributions for (removing NaNs)
-				hotel_names = [ hotel_name for hotel_name in data['Hotel Name'].unique() if not pd.isnull(hotel_name) ]
+			# Get a list of the names of the hotels we aim to plot distributions for (removing NaNs)
+			hotel_names = [ hotel_name for hotel_name in data['Hotel Name'].unique() if not pd.isnull(hotel_name) ]
 
-				# Get the pick-up (drop-off) coordinates of the trip which ended (began) near this each hotel
-				if key == 'pickups':
-					coords = pool.map(get_nearby_pickups_window, [ (data.loc[data['Hotel Name'] == hotel_name], distance, 
-						datetime.combine(date, datetime.min.time()), datetime.combine(date, datetime.max.time())) for hotel_name in hotel_names ])
-				elif key == 'dropoffs':
-					coords = pool.map(get_nearby_dropoffs_window, [ (data.loc[data['Hotel Name'] == hotel_name], distance, 
-						datetime.combine(date, datetime.min.time()), datetime.combine(date, datetime.max.time())) for hotel_name in hotel_names ])
-				
-				coords = { hotel_name : coord for (hotel_name, coord) in zip(hotel_names, coords) }
+			# Get the pick-up (drop-off) coordinates of the trip which ended (began) near this each hotel
+			if key == 'pickups':
+				coords = pool.map(get_nearby_pickups_window, [ (data.loc[data['Hotel Name'] == hotel_name], distance, 
+					datetime.combine(date, datetime.min.time()), datetime.combine(date, datetime.max.time())) for hotel_name in hotel_names ])
+			elif key == 'dropoffs':
+				coords = pool.map(get_nearby_dropoffs_window, [ (data.loc[data['Hotel Name'] == hotel_name], distance, 
+					datetime.combine(date, datetime.min.time()), datetime.combine(date, datetime.max.time())) for hotel_name in hotel_names ])
+			
+			coords = { hotel_name : coord for (hotel_name, coord) in zip(hotel_names, coords) }
 
-				print 'Total satisfying nearby', key, ':', sum([single_hotel_coords.shape[1] \
-													for single_hotel_coords in coords.values()]), '/', len(data), '\n'
-				
-				print 'Satisfying nearby', key, 'by hotel:'
-				for name in coords:
-					print '-', name, ':', coords[name].shape[1], 'satisfying taxicab rides'
+			print 'Total satisfying nearby', key, ':', sum([single_hotel_coords.shape[1] \
+												for single_hotel_coords in coords.values()]), '/', len(data), '\n'
+			
+			print 'Satisfying nearby', key, 'by hotel:'
+			for name in coords:
+				print '-', name, ':', coords[name].shape[1], 'satisfying taxicab rides'
 
-				all_coords.update(coords)
+			all_coords.update(coords)
 
-			coords = all_coords
+		coords = all_coords
 
-			print '\nIt took', timeit.default_timer() - start_time, 'seconds to find all criteria-satifying taxicab trips (for day ' + str(date) + ').\n'
+		print '\nIt took', timeit.default_timer() - start_time, 'seconds to find all criteria-satifying taxicab trips (for day ' + str(date) + ').\n'
 
-			directory = '_'.join([ '_'.join(taxi_data.keys()), str(distance), str(start_date), str(end_date) ])
+		directory = '_'.join([ '_'.join(taxi_data.keys()), str(distance), datetime.combine(date, datetime.min.time()), datetime.combine(date, datetime.max.time()) ])
 
-			combined_coords = (np.concatenate([ coords[hotel_name][0] for hotel_name in coords.keys() ]), \
-										np.concatenate([ coords[hotel_name][1] for hotel_name in coords.keys() ]))
+		combined_coords = (np.concatenate([ coords[hotel_name][0] for hotel_name in coords.keys() ]), \
+									np.concatenate([ coords[hotel_name][1] for hotel_name in coords.keys() ]))
 
-			# Plot a scatterplot for the satisfying coordinates of all hotels combined.
-			plot_arcgis_nyc_scatter_plot(combined_coords, '_'.join([ '_'.join(taxi_data.keys()), str(distance), str(datetime.combine(date, datetime.min.time())), 
-					str(datetime.combine(date, datetime.max.time())) ]), os.path.join(plots_path, directory), title='All Hotels')
+		# Plot a scatterplot for the satisfying coordinates of all hotels combined.
+		plot_arcgis_nyc_scatter_plot(combined_coords, '_'.join([ '_'.join(taxi_data.keys()), str(distance), str(datetime.combine(date, datetime.min.time())), 
+				str(datetime.combine(date, datetime.max.time())) ]), os.path.join(plots_path, directory), title='All Hotels')
 
-			combined_coords = [ (lat, lon) for (lat, lon) in zip(list(combined_coords[0]), list(combined_coords[1])) ]
+		combined_coords = [ ' '.join([str(lat), str(lon)]) for (lat, lon) in zip(list(combined_coords[0]), list(combined_coords[1])) ]
 
-			# Writing this day's satisfying coordinates to a .csv file
-			print '\n...Saving this day\'s satisfying coordinates to disk.' 
-			f.write(', '.join( ['(' + str(coord[0]) + ', ' + str(coord[1]) + ')' for coord in combined_coords ]))
+		print '\n...Writing this day\'s (' + str(date) + ') satisfying coordinates to file.'
+
+		# Writing this day's satisfying coordinates to a .csv file
+		ws.append(combined_coords)
+
+		del combined_coords
 
 
 if __name__ == '__main__':
