60191ffccafa49380c97984526565db19af392b4
djsaunde <djsaunde@umass.edu>
2017-09-21 09:49:14 -0400

    'get_daily_coordinates.py' now uses constant memory (thanks to xlsxwriter) to write satisfying taxi trip data to an Excel file.

--- code/get_daily_coordinates.py
+++ code/get_daily_coordinates.py
@@ -6,6 +6,8 @@ import pandas as pd
 import multiprocessing as mp
 
 from util import *
+from tqdm import tqdm
+from contextlib import closing
 from datetime import timedelta, date, datetime
 
 data_path = os.path.join('..', 'data', 'preprocessed_100')
@@ -18,30 +20,29 @@ for directory in [ output_path, plots_path ]:
 
 
 def plot_and_record_daily(taxi_data, start_date, end_date):
-	wb = xlsxwriter.Workbook(os.path.join(output_path, '_'.join([ '_'.join(taxi_data.keys()), str(distance), start_date, end_date ])), {'constant_memory': True})
+	workbook = xlsxwriter.Workbook(os.path.join(output_path, '_'.join([ '_'.join(taxi_data.keys()), str(distance), str(start_date), str(end_date) ])), {'constant_memory': True})
+	worksheet = workbook.add_worksheet()
 
 	# set up main loop: loop through each day from start_date to end_date
-	for idx, date in enumerate(daterange(start_date, end_date)):
-		start_time = timeit.default_timer()
+	for day_idx, date in enumerate(daterange(start_date, end_date)):
 
 		# get coordinates of new distance and time-constraint satisfying taxicab trips with nearby pick-ups
 		all_coords = {}
 		for key in taxi_data.keys():
 			data = taxi_data[key]
 
-			# Create a number of CPU workers equal to the number of hotels in the data
-			pool = mp.Pool(n_jobs)
-
 			# Get a list of the names of the hotels we aim to plot distributions for (removing NaNs)
 			hotel_names = [ hotel_name for hotel_name in data['Hotel Name'].unique() if not pd.isnull(hotel_name) ]
 
 			# Get the pick-up (drop-off) coordinates of the trip which ended (began) near this each hotel
 			if key == 'pickups':
-				coords = pool.map(get_nearby_pickups_window, [ (data.loc[data['Hotel Name'] == hotel_name], distance, 
-					datetime.combine(date, datetime.min.time()), datetime.combine(date, datetime.max.time())) for hotel_name in hotel_names ])
+				with closing(mp.Pool(n_jobs)) as pool:
+					coords = pool.map(get_nearby_pickups_window, [ (data.loc[data['Hotel Name'] == hotel_name], distance, 
+						datetime.combine(date, datetime.min.time()), datetime.combine(date, datetime.max.time())) for hotel_name in hotel_names ])
 			elif key == 'dropoffs':
-				coords = pool.map(get_nearby_dropoffs_window, [ (data.loc[data['Hotel Name'] == hotel_name], distance, 
-					datetime.combine(date, datetime.min.time()), datetime.combine(date, datetime.max.time())) for hotel_name in hotel_names ])
+				with closing(mp.Pool(n_jobs)) as pool:
+					coords = pool.map(get_nearby_dropoffs_window, [ (data.loc[data['Hotel Name'] == hotel_name], distance, 
+						datetime.combine(date, datetime.min.time()), datetime.combine(date, datetime.max.time())) for hotel_name in hotel_names ])
 			
 			coords = { hotel_name : coord for (hotel_name, coord) in zip(hotel_names, coords) }
 
@@ -52,29 +53,29 @@ def plot_and_record_daily(taxi_data, start_date, end_date):
 			for name in coords:
 				print '-', name, ':', coords[name].shape[1], 'satisfying taxicab rides'
 
+			print '\n'
+
 			all_coords.update(coords)
 
 		coords = all_coords
 
-		print '\nIt took', timeit.default_timer() - start_time, 'seconds to find all criteria-satifying taxicab trips (for day ' + str(date) + ').\n'
-
-		directory = '_'.join([ '_'.join(taxi_data.keys()), str(distance), datetime.combine(date, datetime.min.time()), datetime.combine(date, datetime.max.time()) ])
+		directory = '_'.join([ '_'.join(taxi_data.keys()), str(distance), str(datetime.combine(date, datetime.min.time())), str(datetime.combine(date, datetime.max.time())) ])
 
 		combined_coords = (np.concatenate([ coords[hotel_name][0] for hotel_name in coords.keys() ]), \
 									np.concatenate([ coords[hotel_name][1] for hotel_name in coords.keys() ]))
 
 		# Plot a scatterplot for the satisfying coordinates of all hotels combined.
 		plot_arcgis_nyc_scatter_plot(combined_coords, '_'.join([ '_'.join(taxi_data.keys()), str(distance), str(datetime.combine(date, datetime.min.time())), 
-				str(datetime.combine(date, datetime.max.time())) ]), os.path.join(plots_path, directory), title='All Hotels')
+				str(datetime.combine(date, datetime.max.time())) ]), os.path.join(plots_path, directory), title='All Hotels on ' + str(datetime))
 
 		combined_coords = [ ' '.join([str(lat), str(lon)]) for (lat, lon) in zip(list(combined_coords[0]), list(combined_coords[1])) ]
 
-		print '\n...Writing this day\'s (' + str(date) + ') satisfying coordinates to file.'
-
 		# Writing this day's satisfying coordinates to a .csv file
-		ws.append(combined_coords)
+		worksheet.write(day_idx, 0, str(date))
+		for data_idx in xrange(len(combined_coords)):
+			worksheet.write(day_idx, data_idx + 1, combined_coords[data_idx])
 
-		del combined_coords
+		del combined_coords, coords, all_coords
 
 
 if __name__ == '__main__':
@@ -84,7 +85,7 @@ if __name__ == '__main__':
 	parser.add_argument('--end_date', type=int, nargs=3, default=[2013, 1, 7], help='The day on which to stop looking for satisfying coordinates.')
 	parser.add_argument('--coord_type', type=str, default='pickups', help='The type of coordinates to look for (one of "pickups", "dropoffs", or "both").')
 	parser.add_argument('--distance', type=int, default=100, help='The distance (in feet) from hotels for which to look for satisfying taxicab trips.')
-	parser.add_argument('--n_jobs', type=int, default=4, help='THe number of CPU cores to use in processing the taxicab data.')
+	parser.add_argument('--n_jobs', type=int, default=1, help='The number of CPU cores to use in processing the taxicab data.')
 
 	args = parser.parse_args()
 	args = vars(args)
