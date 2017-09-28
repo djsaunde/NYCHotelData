69c55df15d92386e9d4e341eb5d987b26a78e1c9
Dan Saunders <djsaunde@umass.edu>
2017-09-28 11:53:40 -0400

    Removing unneeded print statement.

--- code/some_preprocess.sh
+++ code/some_preprocess.sh
@@ -1,8 +1,6 @@
 distance=${1:-100}
 
-echo $PWD
-
-declare -a fnames=("yellow_tripdata_2016-05.csv", "yellow_tripdata_2015-11.csv", "yellow_tripdata_2015-08.csv", "yellow_tripdata_2015-05.csv", "yellow_tripdata_2014-08.csv")
+declare -a fnames=("yellow_tripdata_2015-05.csv", "yellow_tripdata_2014-08.csv")
 
 for fname in "${fnames[@]}"
 do
