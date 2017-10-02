dfd1ae07abeaebab1c2a2a9e0035b20501c2db0a
Dan Saunders <djsaunde@umass.edu>
2017-10-02 10:34:58 -0400

    Changing bash script flag for new swarm2 restrictions.

--- code/run_combine_nearby.sh
+++ code/run_combine_nearby.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --partition=longq
 #SBATCH --time=48:00:00
-#SBATCH --mem=100000
+#SBATCH --mem=60000
 #SBATCH --account=rkozma
 #SBATCH --ntasks-per-node=1
 #SBATCH --output=/mnt/nfs/work1/rkozma/djsaunde/NYCHotelData/code/job_reports/%j.out
