Fixed bug with Vincenty formula returning None for calculations which didn't converge.

code/util.py
@@ -23,22 +23,6 @@ import matplotlib.colors as mcolors
 import math, itertools, timeit, multiprocessing, os, multiprocess
 
 
-class BatchCompletionCallBack(object):
-	completed = defaultdict(int)
-
-	def __init__(self, time, index, parallel):
-		self.index = index
-		self.parallel = parallel
-
-	def __call__(self, index):
-		BatchCompletionCallBack.completed[self.parallel] += 1
-		print("done with {}".format(BatchCompletionCallBack.completed[self.parallel]))
-		if self.parallel._original_iterator is not None:
-			self.parallel.dispatch_next()
-
-import joblib.parallel
-joblib.parallel.BatchCompletionCallBack = BatchCompletionCallBack
-
 # print out entire numpy arrays
 np.set_printoptions(threshold=np.nan)
 
@@ -388,16 +372,6 @@ def other_vars_same(prop_row, manhattan_row, capacity):
 	if not (row1_size == 1 and row2_size < 75 or row1_size == 2 and row2_size >= 75 and row2_size <= 149 or row1_size == 3 and row2_size >= 150 and row2_size <= 299 or row1_size == 4 and row2_size >= 300 and row2_size <= 500 or row1_size == 5 and row2_size > 500):
 		return False
 	
-	# checking that the rows match on year opened / date open
-	# if not math.isnan(prop_row['OpenDate']) and manhattan_row['Open Date'] != '    -  -  ':
-	#    row1_year, row2_year = int(prop_row['OpenDate']), int(manhattan_row['Open Date'][0:4])
-	#    if not row1_year == row2_year:
-	#        return False
-	
-	# checking that the capacity of the masked hotel matches the capacity of the hotel from the given row
-	# if not int(capacity) == manhattan_row['Rooms']:
-	#    return False
-	
 	# the rows match on all constraints; therefore, return True
 	return True
 
@@ -429,7 +403,6 @@ def get_hotel_coords(attr_coords, distances):
 	output:
 		The coordinates (latitude, longitude) of the hotel in question.
 	'''
-	
 	# try each permutation of the distances
 	for perm in itertools.permutations(distances):
 		# calculate intersection point
@@ -450,7 +423,6 @@ def get_radius(cx, cy, px, py):
 	output:
 		The radius of the circle.
 	'''
-	
 	dx = px - cx
 	dy = py - cy
 	return math.sqrt(dx ** 2 + dy ** 2)
@@ -467,7 +439,6 @@ def coords_to_distance_miles(start_coords, end_coords):
 	output:
 		The distance between the two points in miles.
 	'''
-	
 	# variables for computing coordinates -> miles
 	R = 6371e3
 	phi_1, phi_2 = math.radians(start_coords[0]), math.radians(end_coords[0])
@@ -491,7 +462,8 @@ def memory_usage_psutil():
 def worker(args):
 	hotel_coords, trip_coords = args
 
-	return [ vincenty(hotel_coords, coord, miles=True) * 5280 for coord in trip_coords ]
+	dists = [ vincenty(hotel_coords, coord, miles=True) for coord in trip_coords ]
+	return [ dist if dist is not None else np.inf for dist in dists ]
 
 
 def get_satisfying_indices(trip_coords, hotel_coords, distance, n_jobs):
@@ -582,7 +554,6 @@ def plot_destinations(destinations, append_to_title):
 	output:
 		Matplotlib plot of datapoints in the range of the latitude, longitude pairs.
 	'''
-
 	plt.plot(destinations[0], destinations[1], 'o')
 	plt.title('Destinations From ' + append_to_title)
 	plt.xlabel('Longitude')