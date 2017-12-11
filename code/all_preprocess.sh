da9f14c21f0e5128ba46dfe0068e4bacaebd8c1f
Dan Saunders <djsaunde@umass.edu>
2017-12-11 12:18:36 -0500

    Fixing bug in bash script which launches preprocessing.

--- code/all_preprocess.sh
+++ code/all_preprocess.sh
@@ -3,7 +3,9 @@ n_jobs=${2:-16}
 
 mkdir "../data/all_preprocessed_$distance"
 
-for i in {0..126}
+n_files=$(ls ../data/taxi_data/ | wc -l)
+
+for (( i=0; i<$n_files; i++ ))
 do
 	sbatch one_preprocess.sh $distance $i $n_jobs
 done
