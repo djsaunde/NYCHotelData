4130837c27422601dedab34115c586fb5fd285da
djsaunde <djsaunde@umass.edu>
2018-03-16 10:56:45 -0400

    Working on paper; adding multiple independent train / test trials.

--- code/naive_predict_occupancy_mlp.py
+++ code/naive_predict_occupancy_mlp.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import os
 import sys
 import argparse
@@ -15,6 +17,7 @@ parser.add_argument('--trip_type', default='pickups', type=str)
 parser.add_argument('--start_date', type=int, nargs=3, default=[2013, 1, 1])
 parser.add_argument('--end_date', type=int, nargs=3, default=[2015, 1, 1])
 parser.add_argument('--metric', type=str, default='rel_diffs')
+parser.add_argument('--trials', type=int, default=5)
 parser.add_argument('--hidden_layer_sizes', nargs='+', type=int, default=[100])
 
 locals().update(vars(parser.parse_args()))
@@ -44,50 +47,62 @@ months = np.array(occupancy['Date'].dt.month).reshape([-1, 1])
 years = np.array(occupancy['Date'].dt.year).reshape([-1, 1])
 targets = np.array(occupancy['Room Demand'])
 
-# Randomly permute the data to remove sequence biasing.
-p = np.random.permutation(targets.shape[0])
-hotels, weekdays, months, years, targets = hotels[p], weekdays[p], months[p], years[p], targets[p]
+train_scores = []
+test_scores = []
+train_mses = []
+test_mses = []
 
-_, hotels = np.unique(hotels, return_inverse=True)
-hotels = hotels.reshape([-1, 1])
+for i in range(trials):  # Run 5 independent realizations of training / test.
+	print('\nTraining, testing model %d / %d' % (i + 1, trials))
+	
+	# Randomly permute the data to remove sequence biasing.
+	p = np.random.permutation(targets.shape[0])
+	hotels, weekdays, months, years, targets = hotels[p], weekdays[p], months[p], years[p], targets[p]
 
-# Split the data into (training, test) subsets.
-split = int(0.8 * len(targets))
+	_, hotels = np.unique(hotels, return_inverse=True)
+	hotels = hotels.reshape([-1, 1])
 
-train_features = [hotels[:split], years[:split], months[:split], weekdays[:split]]
-train_features = np.concatenate(train_features, axis=1)
+	# Split the data into (training, test) subsets.
+	split = int(0.8 * len(targets))
 
-test_features = [hotels[split:], years[split:], months[split:], weekdays[split:]]
-test_features = np.concatenate(test_features, axis=1)
+	train_features = [hotels[:split], years[:split], months[:split], weekdays[:split]]
+	train_features = np.concatenate(train_features, axis=1)
 
-train_targets = targets[:split]
-test_targets = targets[split:]
+	test_features = [hotels[split:], years[split:], months[split:], weekdays[split:]]
+	test_features = np.concatenate(test_features, axis=1)
 
-print('\nCreating and training multi-layer perceptron regression model.\n')
+	train_targets = targets[:split]
+	test_targets = targets[split:]
 
-model = MLPRegressor(verbose=True, hidden_layer_sizes=hidden_layer_sizes).fit(train_features, train_targets)
+	print('Creating and training multi-layer perceptron regression model.')
 
-print('\nTraining complete. Getting predictions and calculating R^2, MSE.')
+	model = MLPRegressor(verbose=True, hidden_layer_sizes=hidden_layer_sizes).fit(train_features, train_targets)
 
-train_score = model.score(train_features, train_targets)
-test_score = model.score(test_features, test_targets)
+	print('Training complete. Getting predictions and calculating R^2, MSE.')
 
-train_predictions = model.predict(train_features)
-train_mse = mean_squared_error(train_targets, train_predictions)
+	train_scores.append(model.score(train_features, train_targets))
+	test_scores.append(model.score(test_features, test_targets))
 
-test_predictions = model.predict(test_features)
-test_mse = mean_squared_error(test_targets, test_predictions)
+	train_predictions = model.predict(train_features)
+	test_predictions = model.predict(test_features)
 
-np.save(os.path.join(predictions_path, 'train_targets.npy'), train_targets)
-np.save(os.path.join(predictions_path, 'train_predictions.npy'), train_predictions)
+	train_mses.append(mean_squared_error(train_targets, train_predictions))
+	test_mses.append(mean_squared_error(test_targets, test_predictions))
 
-np.save(os.path.join(predictions_path, 'test_targets.npy'), test_targets)
-np.save(os.path.join(predictions_path, 'test_predictions.npy'), test_predictions)
+	np.save(os.path.join(predictions_path, 'train_targets_%d.npy' % i), train_targets)
+	np.save(os.path.join(predictions_path, 'train_predictions_%d.npy' % i), train_predictions)
+
+	np.save(os.path.join(predictions_path, 'test_targets_%d.npy' % i), test_targets)
+	np.save(os.path.join(predictions_path, 'test_predictions_%d.npy' % i), test_predictions)
 
 print('\n')
-print('Training R^2:', train_score)
-print('Training MSE:', train_mse)
+print('Mean training MSE:', np.mean(train_mses))
+print('Standard deviation training MSE:', np.std(train_mses))
+print('Mean training R^2:', np.mean(train_scores))
+print('Standard deviation training R^2:', np.std(train_scores))
 print('\n')
-print('Test R^2:', test_score)
-print('Test MSE:', test_mse)
+print('Mean test MSE:', np.mean(test_mses))
+print('Standard deviation test MSE:', np.std(test_mses))
+print('Mean test R^2:', np.mean(test_scores))
+print('Standard deviation test R^2:', np.std(test_scores))
 print('\n')
