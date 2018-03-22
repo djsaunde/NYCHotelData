cb9009082e552d18591c008ef3a32e14e935054c
djsaunde <djsaunde@umass.edu>
2018-03-22 15:33:26 -0400

    Adding MLP grid search for optimization-based removal.

---
+++ code/bash/opt_remove_taxi_mlp_grid_search.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#SBATCH --partition=longq
+#SBATCH --time=48:00:00
+#SBATCH --mem=64000
+#SBATCH --account=rkozma
+#SBATCH --output=/mnt/nfs/work1/rkozma/djsaunde/nyctaxi/job_reports/opt_remove_taxi_mlp_grid_search_%j.out
+#SBATCH --ntasks-per-node=56
+
+distance=${1:-100}
+trip_type=${2:-pickups}
+removals=${3:-15}
+
+cd ..
+
+python grid_search_taxi_opt_remove_mlp.py --distance $distance --trip_type $trip_type --removals $removals
