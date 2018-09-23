063fb8a935cc7c0a38fdcf92050333b4618c40f6
Dan Saunders <djsaunde@umass.edu>
2018-09-23 19:53:50 -0400

    Upgrading some files to Python 3.x; fixing data files without occupancy data; etc./

--- nyctaxi/combine_daily_coordinates.py
+++ nyctaxi/combine_daily_coordinates.py
@@ -5,44 +5,47 @@ import csv
 import argparse
 
 from util import daterange
-from datetime import timedelta, date, datetime
+from datetime import date
 
 output_path = os.path.join('..', 'data', 'daily_distributions')
 
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--start_date', type=int, nargs=3, default=[2013, 1, 1], help='The day on \
-												which to start looking for satisfying coordinates.')
-	parser.add_argument('--end_date', type=int, nargs=3, default=[2013, 1, 5], help='The day on \
-												which to stop looking for satisfying coordinates.')
-	parser.add_argument('--coord_type', type=str, default='pickups', help='The type of coordinates \
-											to look for (one of "pickups", "dropoffs", or "both").')
-	parser.add_argument('--distance', type=int, default=100, help='The distance (in feet) from hotels \
-													for which to look for satisfying taxicab trips.')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--start_date', type=int, nargs=3, default=[2013, 1, 1], help='The day on \
+                                                which to start looking for satisfying coordinates.')
+    parser.add_argument('--end_date', type=int, nargs=3, default=[2013, 1, 5], help='The day on \
+                                                which to stop looking for satisfying coordinates.')
+    parser.add_argument('--coord_type', type=str, default='pickups', help='The type of coordinates \
+                                            to look for (one of "pickups", "dropoffs", or "both").')
+    parser.add_argument('--distance', type=int, default=100, help='The distance (in feet) from hotels \
+                                                    for which to look for satisfying taxicab trips.')
+    args = parser.parse_args()
 
-	args = vars(parser.parse_args())
-	locals().update(args)
+    start_date = args.start_date
+    end_date = args.end_date
+    coord_type = args.coord_type
+    distance = args.distance
 
-	if coord_type == 'pickups':
-		coord_types = ['pickups']
-	elif coord_type == 'dropoffs':
-		coord_types = ['dropoffs']
-	elif coord_type == 'both':
-		coord_types = ['pickups', 'dropoffs']
+    if coord_type == 'pickups':
+        coord_types = ['pickups']
+    elif coord_type == 'dropoffs':
+        coord_types = ['dropoffs']
+    elif coord_type == 'both':
+        coord_types = ['pickups', 'dropoffs']
 
-	start_date, end_date = date(*start_date), date(*end_date)
+    start_date, end_date = date(*start_date), date(*end_date)
 
-	fname = '_'.join([ '_'.join(coord_types), str(distance), str(start_date), str(end_date) ]) + '.csv'
-	with open(os.path.join(output_path, fname), 'w') as to_write:
-		writer = csv.writer(to_write)
+    fname = '_'.join([coord_type, str(distance), str(start_date), str(end_date)]) + '.csv'
+    with open(os.path.join(output_path, fname), 'w') as to_write:
+        writer = csv.writer(to_write)
 
-		for day_idx, date in enumerate(daterange(start_date, end_date)):
-			print('\n*** Date:', date, '***\n')
+        for day_idx, date in enumerate(daterange(start_date, end_date)):
+            print('\n*** Date:', date, '***\n')
 
-			current_fname = '_'.join([ '_'.join(coord_types), str(distance), str(date) ]) + '.csv'
-			with open(os.path.join(output_path, current_fname), 'r') as to_read:
-				reader = csv.reader(to_read)
-				for row in reader:
-					if len(row) != 0:
-						writer.writerow([date] + row)
+            current_fname = '_'.join(['_'.join(coord_types), str(distance), str(date)]) + '.csv'
+            with open(os.path.join(output_path, current_fname), 'r') as to_read:
+                reader = csv.reader(to_read)
+                for row in reader:
+                    if len(row) != 0:
+                        writer.writerow([date] + row)
--- nyctaxi/ml/utils.py
+++ nyctaxi/ml/utils.py
@@ -1,174 +1,198 @@
 import os
-import numpy  as np
+import numpy as np
 import pandas as pd
 
-from time                 import time
+from time import time
 from sklearn.linear_model import LinearRegression
 
 
 class LogitRegression(LinearRegression):
-	
-	def fit(self, x, p):
-		p = np.asarray(p)
-		p[p == 0.0] = 1e-8
-		p[p == 1.0] = 1 - 1e-2
-		y = np.log(p / (1 - p))
-		return super().fit(x, y)
+    def fit(self, x, p):
+        p = np.asarray(p)
+        p[p == 0.0] = 1e-8
+        p[p == 1.0] = 1 - 1e-2
+        y = np.log(p / (1 - p))
+        return super().fit(x, y)
 
-	def predict(self, x):
-		y = super().predict(x)
-		return 1 / (np.exp(-y) + 1)
+    def predict(self, x):
+        y = super().predict(x)
+        return 1 / (np.exp(-y) + 1)
 
 
 def load_occupancy_data(data_path, start_date, end_date):
-	print('\nLoading daily per-hotel capacity data.'); start = time()
+    print('\nLoading daily per-hotel capacity data.'); start = time()
 
-	# Load daily capacity data.
-	df = pd.read_csv(os.path.join(data_path, 'Unmasked Capacity and Price Data.csv'), index_col=False)
-	df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
-	df['ADR'] = df['ADR'].astype(str).str.replace(',', '')
-	df['Room Demand'] = df['Room Demand'].astype(str).str.replace(',', '')
-	df['Occ'] = df['Occ'] / 100
-	df = df.loc[(df['Date'] >= start_date) & (df['Date'] <= end_date)]
-	df = df.rename(index=str, columns={'Share ID' : 'Hotel Name'})
-	
-	print('Time: %.4f' % (time() - start))
-	
-	return df
+    # Load daily capacity data.
+    df = pd.read_csv(os.path.join(data_path, 'Unmasked Capacity and Price Data.csv'), index_col=False)
+    df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+    df['ADR'] = df['ADR'].astype(str).str.replace(',', '')
+    df['Room Demand'] = df['Room Demand'].astype(str).str.replace(',', '')
+    df['Occ'] = df['Occ'] / 100
+    df = df.loc[(df['Date'] >= start_date) & (df['Date'] <= end_date)]
+    df = df.rename(index=str, columns={'Share ID' : 'Hotel Name'})
+
+    print('Time: %.4f' % (time() - start))
+
+    return df
+
 
 def load_merged_data(data_path, taxi_occupancy_path, preproc_data_path, start_date, end_date, trip_type):
-	data_fname = 'Taxi occupancy price data.csv'
-	counts_fname = 'Taxi occupancy price counts.csv'
-	
-	is_data_file = os.path.isfile(os.path.join(taxi_occupancy_path, data_fname))
-	is_counts_file = os.path.isfile(os.path.join(taxi_occupancy_path, counts_fname))
-	
-	if not is_counts_file and not is_data_file:
-		# Load daily capacity data.
-		print('\nLoading daily per-hotel capacity data.'); start = time()
+    data_fname = 'Taxi occupancy price data.csv'
+    counts_fname = 'Taxi occupancy price counts.csv'
 
-		occupancy = pd.read_csv(os.path.join(data_path, 'Unmasked Capacity and Price Data.csv'), index_col=False)
-		occupancy['Date'] = pd.to_datetime(occupancy['Date'], format='%Y-%m-%d')
-		occupancy = occupancy.loc[(occupancy['Date'] >= start_date) & (occupancy['Date'] <= end_date)]
-		occupancy['Date'] = occupancy['Date'].dt.date
-		occupancy['ADR'] = occupancy['ADR'].astype(str).str.replace(',', '')
-		occupancy['Room Demand'] = occupancy['Room Demand'].astype(str).str.replace(',', '')
-		occupancy['Occ'] = occupancy['Occ'] / 100
-		occupancy = occupancy.rename(index=str, columns={'Share ID': 'Hotel Name'})
-		occupancy = occupancy.drop('Unnamed: 0', axis=1)
+    is_data_file = os.path.isfile(os.path.join(taxi_occupancy_path, data_fname))
+    is_counts_file = os.path.isfile(os.path.join(taxi_occupancy_path, counts_fname))
 
-		print('Time: %.4f' % (time() - start))
+    if not is_counts_file and not is_data_file:
+        # Load daily capacity data.
+        print('\nLoading daily per-hotel capacity data.'); start = time()
 
-		# Load preprocessed data according to command-line "distance" parameter.
-		print('\nReading in the pre-processed taxicab data.'); start = time()
+        occupancy = pd.read_csv(os.path.join(data_path, 'Unmasked Capacity and Price Data.csv'), index_col=False)
+        occupancy['Date'] = pd.to_datetime(occupancy['Date'], format='%Y-%m-%d')
+        occupancy = occupancy.loc[(occupancy['Date'] >= start_date) & (occupancy['Date'] <= end_date)]
+        occupancy['Date'] = occupancy['Date'].dt.date
+        occupancy['ADR'] = occupancy['ADR'].astype(str).str.replace(',', '')
+        occupancy['Room Demand'] = occupancy['Room Demand'].astype(str).str.replace(',', '')
+        occupancy['Occ'] = occupancy['Occ'] / 100
+        occupancy = occupancy.rename(index=str, columns={'Share ID': 'Hotel Name'})
+        occupancy = occupancy.drop('Unnamed: 0', axis=1)
 
-		usecols = ['Hotel Name', 'Pick-up Time', 'Drop-off Time', 'Distance From Hotel']
-		if trip_type == 'pickups':
-			filename = os.path.join(preproc_data_path, 'destinations.csv')
-		elif trip_type == 'dropoffs':
-			filename = os.path.join(preproc_data_path, 'starting_points.csv')
-		else:
-			raise Exception('Expecting one of "pickups" or "dropoffs" for command-line argument "trip_type".')
+        print('Time: %.4f' % (time() - start))
 
-		taxi_rides = pd.read_csv(filename, header=0, usecols=usecols)
+        # Load preprocessed data according to command-line "distance" parameter.
+        print('\nReading in the pre-processed taxicab data.'); start = time()
 
-		taxi_rides['Hotel Name'] = taxi_rides['Hotel Name'].apply(str.strip)
-		taxi_rides['Pick-up Time'] = pd.to_datetime(taxi_rides['Pick-up Time'], format='%Y-%m-%d')
-		taxi_rides['Drop-off Time'] = pd.to_datetime(taxi_rides['Drop-off Time'], format='%Y-%m-%d')
-		taxi_rides = taxi_rides.loc[(taxi_rides['Pick-up Time'] >= start_date) & \
-										(taxi_rides['Drop-off Time'] <= end_date)]
-		taxi_rides['Date'] = taxi_rides['Pick-up Time'].dt.date
-		taxi_rides = taxi_rides.drop(['Pick-up Time', 'Drop-off Time'], axis=1)
+        usecols = ['Hotel Name', 'Pick-up Time', 'Drop-off Time', 'Distance From Hotel']
+        if trip_type == 'pickups':
+            filename = os.path.join(preproc_data_path, 'destinations.csv')
+        elif trip_type == 'dropoffs':
+            filename = os.path.join(preproc_data_path, 'starting_points.csv')
+        else:
+            raise Exception('Expecting one of "pickups" or "dropoffs" for command-line argument "trip_type".')
 
-		print('Time: %.4f' % (time() - start))
+        taxi_rides = pd.read_csv(filename, header=0, usecols=usecols)
 
-		# Build the dataset of ((hotel, taxi density), occupancy) input, output pairs.
-		print('\nMerging dataframes on Date and Hotel Name attributes.'); start = time()
+        taxi_rides['Hotel Name'] = taxi_rides['Hotel Name'].apply(str.strip)
+        taxi_rides['Pick-up Time'] = pd.to_datetime(taxi_rides['Pick-up Time'], format='%Y-%m-%d')
+        taxi_rides['Drop-off Time'] = pd.to_datetime(taxi_rides['Drop-off Time'], format='%Y-%m-%d')
+        taxi_rides = taxi_rides.loc[
+            (taxi_rides['Pick-up Time'] >= start_date) & (taxi_rides['Drop-off Time'] <= end_date)
+        ]
+        taxi_rides['Date'] = taxi_rides['Pick-up Time'].dt.date
+        taxi_rides = taxi_rides.drop(['Pick-up Time', 'Drop-off Time'], axis=1)
 
-		df = pd.merge(occupancy, taxi_rides, on=['Date', 'Hotel Name'])
+        print('Time: %.4f' % (time() - start))
 
-		print('Time: %.4f' % (time() - start))
+        # Build the dataset of ((hotel, taxi density), occupancy) input, output pairs.
+        print('\nMerging dataframes on Date and Hotel Name attributes.'); start = time()
 
-		# Save merged occupancy and taxi data to disk.
-		print('\nSaving merged dataframes to disk.'); start = time()
+        df = pd.merge(occupancy, taxi_rides, on=['Date', 'Hotel Name'])
 
-		df.to_csv(os.path.join(taxi_occupancy_path, data_fname))
+        print('Time: %.4f' % (time() - start))
 
-		print('Time: %.4f' % (time() - start))
+        # Save merged occupancy and taxi data to disk.
+        print('\nSaving merged dataframes to disk.'); start = time()
 
-		# Count number of rides per hotel and date.
-		df = df.groupby(['Hotel Name', 'Date', 'Room Demand', 'ADR', 'Occ']).count().reset_index()
-		df = df.rename(index=str, columns={'Distance From Hotel': 'No. Nearby Trips'})
-		df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+        df.to_csv(os.path.join(taxi_occupancy_path, data_fname))
 
-		# Save occupancy and taxi counts to disk.
-		print('\nSaving counts to disk.'); start = time()
+        print('Time: %.4f' % (time() - start))
 
-		df.to_csv(os.path.join(taxi_occupancy_path, counts_fname))
+        # Count number of rides per hotel and date.
+        df = df.groupby(['Hotel Name', 'Date', 'Room Demand', 'ADR', 'Occ']).count().reset_index()
+        df = df.rename(index=str, columns={'Distance From Hotel': 'No. Nearby Trips'})
+        df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
 
-		print('Time: %.4f' % (time() - start))
+        # Save occupancy and taxi counts to disk.
+        print('\nSaving counts to disk.'); start = time()
 
-	elif is_data_file and not is_counts_file:
-		# Load merged occupancy and taxi data to disk.
-		print('\nLoading merged taxi and occupancy dataframes from disk.'); start = time()
+        df.to_csv(os.path.join(taxi_occupancy_path, counts_fname))
 
-		df = pd.read_csv(os.path.join(taxi_occupancy_path, data_fname))
+        print('Time: %.4f' % (time() - start))
 
-		print('Time: %.4f' % (time() - start))
+    elif is_data_file and not is_counts_file:
+        # Load merged occupancy and taxi data to disk.
+        print('\nLoading merged taxi and occupancy dataframes from disk.'); start = time()
 
-		# Count number of rides per hotel and date.
-		df = df.groupby(['Hotel Name', 'Date', 'Room Demand', 'ADR', 'Occ']).count().reset_index()
-		df = df.rename(index=str, columns={'Distance From Hotel': 'No. Nearby Trips'})
-		df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+        df = pd.read_csv(os.path.join(taxi_occupancy_path, data_fname))
+        df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+        df['Date'] = df['Date'].dt.date
 
-		# Save occupancy and taxi counts to disk.
-		print('\nSaving counts to disk.'); start = time()
+        print('Time: %.4f' % (time() - start))
 
-		df.to_csv(os.path.join(taxi_occupancy_path, counts_fname))
+        # Make sure occupancy is in the merged occupancy and taxi data.
+        if 'Occ' not in df.columns:
+            occupancy = pd.read_csv(os.path.join(data_path, 'Unmasked Capacity and Price Data.csv'), index_col=False)
+            occupancy['Date'] = pd.to_datetime(occupancy['Date'], format='%Y-%m-%d')
+            occupancy = occupancy.loc[(occupancy['Date'] >= start_date) & (occupancy['Date'] <= end_date)]
+            occupancy['Date'] = occupancy['Date'].dt.date
+            occupancy['ADR'] = occupancy['ADR'].astype(str).str.replace(',', '')
+            occupancy['Room Demand'] = occupancy['Room Demand'].astype(str).str.replace(',', '')
+            occupancy['Occ'] = occupancy['Occ'] / 100
+            occupancy = occupancy.rename(index=str, columns={'Share ID': 'Hotel Name'})
+            occupancy = occupancy[['Date', 'Hotel Name', 'Occ']]
 
-		print('Time: %.4f' % (time() - start))
+            df = pd.merge(occupancy, df, on=['Date', 'Hotel Name'])
 
-	else:
-		# Load merged occupancy and taxi data from disk.
-		print('\nLoading occupancy and taxi counts from disk.'); start = time()
+        # Count number of rides per hotel and date.
+        df = df.groupby(['Hotel Name', 'Date', 'Room Demand', 'ADR', 'Occ']).count().reset_index()
+        df = df.rename(index=str, columns={'Distance From Hotel': 'No. Nearby Trips'})
+        df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
 
-		df = pd.read_csv(os.path.join(taxi_occupancy_path, counts_fname))
-		df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+        # Save occupancy and taxi counts to disk.
+        print('\nSaving counts to disk.'); start = time()
 
-		print('Time: %.4f' % (time() - start))
-	
-	return df
+        df.to_csv(os.path.join(taxi_occupancy_path, counts_fname))
 
-def encode_data(df, targets=['Occ']):
-	# One-hot encoding the hotel IDs.
-	hotels = np.array(df['Hotel Name'])
-	hotel_names, hotels = np.unique(hotels, return_inverse=True)
-	hotels = [hotels == i for i in range(max(hotels) + 1)]
+        print('Time: %.4f' % (time() - start))
 
-	# Get the trip counts.
-	if 'No. Nearby Trips' in df:
-		print('*** Utilizing nearby taxi trip counts. ***')
-		trips = list(df['No. Nearby Trips'])
+    else:
+        # Load merged occupancy and taxi data from disk.
+        print('\nLoading occupancy and taxi counts from disk.'); start = time()
 
-	# One-hot encoding the day of the week.
-	weekdays = np.array(df['Date'].dt.weekday)
-	weekdays = [weekdays == i for i in range(7)]
+        df = pd.read_csv(os.path.join(taxi_occupancy_path, counts_fname))
+        df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
 
-	# One-hot encoding the month.
-	months = np.array(df['Date'].dt.month)
-	months = [months == i for i in range(1, 13)]
+        print('Time: %.4f' % (time() - start))
 
-	# One-hot encoding the year.
-	years = np.array(df['Date'].dt.year)
-	years = [years == i for i in range(2014, 2017)]
-	
-	# Combining all observations into a design matrix.
-	if 'No. Nearby Trips' in df:
-		observations = np.array(hotels + weekdays + months + years + [trips]).T
-	else:
-		observations = np.array(hotels + weekdays + months + years).T
+    return df
 
-	# Get the target outputs (occupancy and room pricing).
-	targets = np.array(df[targets], dtype=np.float32)
-	
-	return observations, targets
+
+def encode_data(df, obs=('IDs', 'Weekdays', 'Months', 'Years'), targets=('Occ', )):
+    observations = []
+
+    # One-hot encoding the hotel IDs.
+    if 'IDs' in obs:
+        hotels = np.array(df['Hotel Name'])
+        hotel_names, hotels = np.unique(hotels, return_inverse=True)
+        hotels = [hotels == i for i in range(max(hotels) + 1)]
+        observations = observations + hotels
+
+    # One-hot encoding the day of the week.
+    if 'Weekdays' in obs:
+        weekdays = np.array(df['Date'].dt.weekday)
+        weekdays = [weekdays == i for i in range(7)]
+        observations = observations + weekdays
+
+    # One-hot encoding the month.
+    if 'Months' in obs:
+        months = np.array(df['Date'].dt.month)
+        months = [months == i for i in range(1, 13)]
+        observations = observations + months
+
+    # One-hot encoding the year.
+    if 'Years' in obs:
+        years = np.array(df['Date'].dt.year)
+        years = [years == i for i in range(2014, 2017)]
+        observations = observations + years
+
+    # Get the trip counts.
+    if 'No. Nearby Trips' in df:
+        print('*** Utilizing nearby taxi trip counts. ***')
+        trips = list(df['No. Nearby Trips'])
+        observations = observations + [trips]
+
+    # Get the observations and target outputs (occupancy and room pricing).
+    observations = np.array(observations).T
+    outputs = np.array(df[list(targets)], dtype=np.float32)
+
+    return observations, outputs
